64209995455ba4b6211d38306a0fa85c21d2e17d
Vitalij Neverov <vitalij.neverov@gmail.com>
2026-03-09 20:27:44 +0100

    Add user.service.ts

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findAll(): Promise<User[]> {
+    return this.prismaService.user.findMany();
+  }
+
+  async findOne(id: number): Promise<User | null> {
+    return this.prismaService.user.findUnique({ where: { id } });
+  }
+
+  async create(dto: CreateUserDto): Promise<User> {
+    return this.prismaService.user.create({ data: dto });
+  }
+}
