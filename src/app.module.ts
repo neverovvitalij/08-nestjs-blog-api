2041d05b1e6210c1b56ae3587427a882c2be7c82
Vitalij Neverov <vitalij.neverov@gmail.com>
2026-03-10 09:17:52 +0100

    Add ValidationPipe and ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UsersModule],
+  imports: [ConfigModule.forRoot(), PrismaModule, UsersModule],
 })
 export class AppModule {}
