046890beb61ddd9c7d1b1cbae17178c9724f6a28
Vitalij Neverov <vitalij.neverov@gmail.com>
2026-03-13 10:52:14 +0100

    Add post.service.ts

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreatePostDto } from 'src/posts/dto/create-post.dto';
+import { Post } from 'src/posts/entities/post.entity';
+import { slugify } from 'src/utils/slugify';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(dto: CreatePostDto): Promise<Post> {
+    const slug = slugify(dto.title);
+    return this.prismaService.post.create({ data: { ...dto, slug } });
+  }
+
+  async getAll(): Promise<Post[]> {
+    return this.prismaService.post.findMany();
+  }
+
+  async findOne(id: number): Promise<Post | null> {
+    return this.prismaService.post.findUnique({ where: { id } });
+  }
+
+  async update(id: number, dto: Partial<CreatePostDto>): Promise<Post> {
+    const data: Prisma.PostUpdateInput = { ...dto };
+
+    if (dto.title) {
+      data.slug = slugify(dto.title);
+    }
+
+    return this.prismaService.post.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async delete(id: number): Promise<Post> {
+    return this.prismaService.post.update({
+      where: { id },
+      data: { deletedAt: new Date() },
+    });
+  }
+}
